9953e9d844703ef9862ef8baf32d6d662dfc04c1
Dan Dumont <ddumont@gmail.com>
2015-08-22 19:39:53 -0400

    update the site font and make sorting more flexible

--- script.js
+++ script.js
@@ -8,54 +8,72 @@ if (
   document.body.className = "unsupported";
 }
 
-if (!window.data) { // let the data load
-  var nodes = Array.prototype.slice.call(document.head.childNodes).filter(function(node) {
-    return node.nodeType === Node.ELEMENT_NODE
-      && node.nodeName === 'SCRIPT'
-      && /data\.js$/.test(node.src);
-  }).forEach(function(node) {
-    node.addEventListener('load', function () {
-       build();
-    }, false);
+(function() {
+  if (!window.data) { // let the data load
+    var nodes = Array.prototype.slice.call(document.head.childNodes).filter(function(node) {
+      return node.nodeType === Node.ELEMENT_NODE
+        && node.nodeName === 'SCRIPT'
+        && /data\.js$/.test(node.src);
+    }).forEach(function(node) {
+      node.addEventListener('load', function () {
+         build();
+      }, false);
+    });
+  } else {
+    build();
+  }
+
+  var local = document.body.querySelector('.time.local');
+  var game = document.body.querySelector('.time.game');
+  var formatter = new Intl.DateTimeFormat(undefined, {
+    hour: 'numeric', minute: 'numeric', timeZone: 'UTC'
   });
-} else {
-  build();
-}
+  var sorts = {
+    'time': function(a, b) {
+      return Number(a.dataset.time) - Number(b.dataset.time);
+    }
+  };
+  try {
+    // Start the web wokrer
+    var worker = new Worker("worker.js");
+    worker.onmessage = function(e) {
+      local.textContent = e.data[1];
+      game.textContent = e.data[2];
+    };
+  } catch(e) {
+    console.error(e);
+  }
 
-var local = document.body.querySelector('.time.local');
-var game = document.body.querySelector('.time.game');
-var formatter = new Intl.DateTimeFormat(undefined, {
-  hour: 'numeric', minute: 'numeric', timeZone: 'UTC'
-});
+  function resort(parent, elems, compare) {
+    elems = Array.prototype.slice.call(elems || list.childNodes || [])
+    .filter(function(node) {
+      return node.nodeType === Node.ELEMENT_NODE;
+    });
+    elems.sort(compare).forEach(function(elem) {
+      parent.appendChild(elem);
+    });
+  }
 
-function build() {
-  mining(data.filter(function(item) {
-    return item.type === 'mining';
-  }));
-}
+  function build() {
+    mining(data.filter(function(item) {
+      return item.type === 'mining';
+    }));
+  }
 
-function mining(data, sort) {
-  sort = sort || 'time'
-  data.sort(function(a, b) {
-    return a[sort] - b[sort];
-  });
+  function mining(data) {
+    var list = document.body.querySelector('section.mining ol.list');
+    list.innerHTML = '';
 
-  var list = document.body.querySelector('section.mining ol.list');
-  list.innerHTML = '';
-
-  var template = document.body.querySelector('template.mining.row');
-  data.forEach(function(item) {
-    template.content.querySelector('.time').textContent =
-      window.formatter.format(new Date(item.time * 1000));
-    template.content.querySelector('.name').textContent = item.name;
-    template.content.querySelector('.location').textContent = item.location;
-    list.appendChild(document.importNode(template.content, true));
-  });
-}
-
-// Start the web wokrer
-var worker = new Worker("worker.js");
-worker.onmessage = function(e) {
-  local.textContent = e.data[1];
-  game.textContent = e.data[2];
-};
+    var content = document.body.querySelector('template.mining.row').content;
+    resort(list, data.map(function(item) {
+      var li = content.querySelector('li');
+      Object.keys(item).forEach(function(key) {
+        li.dataset[key] = item[key];
+      });
+      li.querySelector('.time').textContent = formatter.format(new Date(item.time * 1000));
+      li.querySelector('.name').textContent = item.name;
+      li.querySelector('.location').textContent = item.location;
+      return document.importNode(content, true).firstElementChild;
+    }), sorts.time);
+  }
+})();
