09669dc7e957749f496b9390210fb41d1150ac9d
Dan Dumont <ddumont@gmail.com>
2015-08-22 15:27:11 -0400

    tick every eorzea minute

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ function tick() {
   self.timeout = setTimeout(function() {
     tick(); // schedule next tick.
     postMessage('tick');
-  }, 1000); // tick every second
+  }, 1000 * 175 / 3600 * 60); // tick every eorzea minute
 }
 
 tick();
