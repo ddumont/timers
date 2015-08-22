23a94954c92d9db13a277a7490816d7a8cb9b19f
Dan Dumont <ddumont@gmail.com>
2015-08-22 16:14:33 -0400

    send the initial tick earlier

--- worker.js
+++ worker.js
@@ -8,14 +8,14 @@ var game = new Intl.DateTimeFormat(undefined, {
 function tick() {
   self.timeout = setTimeout(function() {
     tick(); // schedule next tick.
+  }, 1000); // tick every second.
 
-    var now = new Date();
-    postMessage([
-      'tick',
-      local.format(now),
-      game.format(new Date(now.valueOf() * 3600 / 175))
-    ]);
-  }, 1000); // tick every second
+  var now = new Date();
+  postMessage([
+    'tick',
+    local.format(now),
+    game.format(new Date(now.valueOf() * 3600 / 175))
+  ]);
 }
 
 tick();
