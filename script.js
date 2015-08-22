8ebb520ff4398d0037c4e214f75695fbe048bc9f
Dan Dumont <ddumont@gmail.com>
2015-08-22 15:03:09 -0400

    display the local time

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 if (
   !window.Worker ||
+  !window.Intl ||
+  !window.Intl.DateTimeFormat ||
   !("classList" in document.createElement("_"))
 ) {
   document.body.className = "unsupported";
 }
 
+var options = {
+  hour: 'numeric', minute: 'numeric', second: 'numeric',
+  timeZoneName: 'short'
+};
+var formatter = new Intl.DateTimeFormat(undefined, options);
+
+var local = document.body.querySelector('.time.local');
+var game = document.body.querySelector('.time.local');
+
 // Start the web wokrer
 var worker = new Worker("worker.js");
 worker.onmessage = function(e) {
-  console.log('Message received from worker', e);
+  local.textContent = formatter.format(new Date());
 };
