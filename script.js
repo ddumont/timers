8326fa2bdd13d034f35c80a15a93eda1be0b6096
Dan Dumont <ddumont@gmail.com>
2015-08-24 00:58:58 -0400

    Some style updates. Last of the hash fixes. Add botany and fishing nodes.

--- script.js
+++ script.js
@@ -29,8 +29,13 @@ if (
     hour: 'numeric', minute: 'numeric', timeZone: 'UTC'
   });
   var sorts = {
-    'time': function(a, b) {
-      return Number(a.dataset.time) - Number(b.dataset.time);
+    'default': function(a, b) {
+      var delta = Number(a.dataset.time) - Number(b.dataset.time);
+      if (delta !== 0) return delta;
+      delta = Number(a.dataset.nodeid) - Number(b.dataset.nodeid);
+      if (delta !== 0) return delta;
+      return Number(a.dataset.slot) - Number(b.dataset.slot);
+
     }
   };
   try {
@@ -62,6 +67,25 @@ if (
     mining(data.filter(function(item) {
       return item.type === 'mining';
     }));
+    botany(data.filter(function(item) {
+      return item.type === 'botany';
+    }));
+    fishing(data.filter(function(item) {
+      return item.type === 'fishing';
+    }));
+
+    Array.prototype.slice.call(
+      document.body.querySelectorAll('li[data-nodeid]')
+    ).forEach(function(node, idx, arr) {
+      node.addEventListener('click', function(e) {
+        var target = e.currentTarget;
+        arr.forEach(function(check) {
+          if (check.dataset.nodeid === target.dataset.nodeid)
+            check.classList.toggle('selected');
+        });
+        hash();
+      });
+    });
 
     unhash();
   }
@@ -81,18 +105,48 @@ if (
       li.querySelector('.name').textContent = item.name;
       li.querySelector('.location').textContent = item.location;
 
-      var elem = document.importNode(content, true).firstElementChild;
-      elem.addEventListener('click', function(e) {
-        Array.prototype.slice.call(
-          document.body.querySelectorAll('[data-nodeid="' + item.nodeid + '"]')
-        ).forEach(function(node) {
-          node.classList.toggle('selected');
-        });
-        hash();
-      });
+      return document.importNode(content, true).firstElementChild;
+    }), sorts.default);
+  }
 
-      return elem;
-    }), sorts.time);
+  function botany(data) {
+    var list = document.body.querySelector('section.botany ol');
+    list.innerHTML = '';
+
+    // use the mining template for now.
+    var content = document.body.querySelector('template.mining.row').content;
+    resort(list, data.map(function(item, idx) {
+      var li = content.querySelector('li');
+      Object.keys(item).forEach(function(key) {
+        li.dataset[key] = item[key];
+      });
+      li.dataset.id = idx;
+      li.querySelector('.time').textContent = formatter.format(new Date(item.time * 1000));
+      li.querySelector('.name').textContent = item.name;
+      li.querySelector('.location').textContent = item.location;
+
+      return document.importNode(content, true).firstElementChild;
+    }), sorts.default);
+  }
+
+  function fishing(data) {
+    var list = document.body.querySelector('section.fishing ol');
+    list.innerHTML = '';
+
+    // use the mining template for now.
+    var content = document.body.querySelector('template.mining.row').content;
+    resort(list, data.map(function(item, idx) {
+      var li = content.querySelector('li');
+      Object.keys(item).forEach(function(key) {
+        li.dataset[key] = item[key];
+      });
+      li.dataset.id = idx;
+      li.querySelector('.time').textContent = formatter.format(new Date(item.time * 1000));
+      li.querySelector('.name').textContent = item.name;
+      li.querySelector('.location').textContent = item.location;
+
+      return document.importNode(content, true).firstElementChild;
+    }), sorts.default);
   }
 
   /**
@@ -101,7 +155,7 @@ if (
   function hash() {
     var data = [];
     Array.prototype.slice.call(
-      document.body.querySelectorAll('section.mining ol li')
+      document.body.querySelectorAll('li[data-nodeid]')
     ).forEach(function(node) {
       if (node.classList.contains('selected')) {
         var id = Number(node.dataset.nodeid)
@@ -110,11 +164,9 @@ if (
         data[idx] = (data[idx] || 0) | (1 << val);
       }
     });
-    var encoded = btoa(data.map(function(num) {
-      return String.fromCharCode(num);
-    }).join(''));
-
-    history.replaceState(undefined, undefined, '#' + encoded);
+    for (var i = 0; i < data.length; i++)
+      data[i] = String.fromCharCode(data[i] || 0);
+    history.replaceState(undefined, undefined, '#' + btoa(data.join('')));
   }
 
   /**
@@ -129,7 +181,7 @@ if (
       for (var j = 8; j > 0; j--) {
         var id = nodeid++;
         if (mask && (mask & 1)) {
-          var nodes = document.body.querySelectorAll('[data-nodeid="' + id + '"]');
+          var nodes = document.body.querySelectorAll('li[data-nodeid="' + id + '"]');
           Array.prototype.slice.call(nodes || []).forEach(function(node) {
             node.classList.add('selected')
           });
