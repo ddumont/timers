f8a060e57fb02cadb92e25bb0205f8ba30422ee3
Dan Dumont <ddumont@gmail.com>
2016-03-24 00:08:11 -0400

    Fix slot for astral moraine

--- app/data.js
+++ app/data.js
@@ -1184,7 +1184,7 @@ export default (function() {
     },{
       "type": "mining",
       "name": "Astral Moraine",
-      "slot": 3,
+      "slot": 4,
       "location": "Coerthas W. Highlands",
       "time": "5:00",
       "duration": HW,
