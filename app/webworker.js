b7acb2bce518c0100bf8720b6b5a4905564bbeb7
Dan Dumont <ddumont@gmail.com>
2016-03-30 22:01:25 -0400

    Remove old service worker compat code

--- app/webworker.js
+++ app/webworker.js
@@ -7,12 +7,7 @@ function tick() {
   const eorzea = new Date(now * 3600 / 175).valueOf();
   const elapsed = Math.floor(eorzea / 1000) % 86400; // # of elapsed seconds in the eorzean day
   const data = ['tick', now, eorzea, elapsed];
-  if (self.clients) {
-    clients.matchAll({includeUncontrolled: true})
-      .then(all => all.map(client => client.postMessage(data)));
-  } else {
-    postMessage(data);
-  }
+  postMessage(data);
 }
 
 tick();
